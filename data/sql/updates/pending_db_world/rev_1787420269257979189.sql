765c45a86066282c1b4780498f1f10367bd4078f
Moostigre <moostigre@proton.me>
2026-09-06 19:37:05 +0200

    style(DB/Stratholme): Wrap Supply Crate script cleanup

--- data/sql/updates/pending_db_world/rev_1787420269257979189.sql
+++ data/sql/updates/pending_db_world/rev_1787420269257979189.sql
@@ -3,4 +3,5 @@ UPDATE `gameobject_template` SET `AIName`='' WHERE `entry` IN (176304,176307,176
 UPDATE `gameobject_template` SET `Data0`=57 WHERE `entry` IN (176304,176307,176308,176309);
 UPDATE `gameobject_template` SET `AIName`='' WHERE `entry` IN (175534,175535,175536,175537);
 UPDATE `gameobject_template_addon` SET `faction`=168 WHERE `entry` IN (175534,175535,175536,175537);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (175534,175535,175536,175537,176304,176307,176308,176309) AND `source_type`=1;
+DELETE FROM `smart_scripts`
+WHERE `entryorguid` IN (175534,175535,175536,175537,176304,176307,176308,176309) AND `source_type`=1;
